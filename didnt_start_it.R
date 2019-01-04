6fe31cd4c89eefd2b1c8b865cc2fd3f0ecd4ce94
aedobbyn <adobbs11@gmail.com>
2019-01-04 13:08:57 -0500

    extract geolocation

--- didnt_start_it.R
+++ didnt_start_it.R
@@ -1,8 +1,14 @@
 
 library(tidyverse)
 library(twitteR)
+library(ggmap)
 
 source(here::here("key.R"))
+register_google(gmaps_key)
+
+boroughs <- c("Brooklyn", "Bronx", "Manhattan", "Staten", "Queens")
+borough_reg <- boroughs %>% 
+  str_c(collapse = "|")
 
 fires_raw <- userTimeline("NYCFireWire")
 
@@ -11,14 +17,46 @@ fires <-
   twListToDF() %>% 
   as_tibble()
 
-fires %>% 
+addresses <-
+  fires %>% 
   mutate(
     borough = str_extract(text, "^[^\\s]*\\s") %>% 
       str_remove("\\s"),
     street = str_extract(text, "(\\*.+[,\\.])") %>% # All text after an asterisk and before a comma or period
-      str_remove_all("(\\*.+\\*)"), # Get rid of stuff in between asterisks
-    address = glue::glue("{street}, {borough}")
+      str_remove_all("(\\*.+\\*)") %>%  # Get rid of stuff in between asterisks
+      str_trim()
+  ) %>%
+  mutate(
+    borough = 
+      case_when(str_detect(borough, borough_reg) ~ borough,
+                TRUE ~ NA_character_),
+    address = 
+      glue::glue("{street}, {borough}", .na = "") %>% 
+      str_remove("[, ]?") %>%  
+      str_trim()
+  ) %>% 
+  mutate(
+    address = na_if(address, "")
   ) %>% 
   select(borough, street, address, text)
+
+geo_to_list <- function(inp) {
+  geocode(inp) %>% list()
+}
+
+# Get lat lon
+lat_long <- 
+  addresses %>% 
+  rowwise() %>% 
+  mutate(
+    l_l = ifelse(is.na(address), tibble(lat = NA_character_, 
+                                        lon = NA_character_) %>% list(), 
+                 geo_to_list(address))
+  ) %>% 
+  unnest() %>% 
+  select(address, lat, lon)
+
+
+
 
 
