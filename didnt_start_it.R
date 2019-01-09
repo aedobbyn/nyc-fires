58fd6f8e7d996e6d6b5742a10a28c217492e2da5
aedobbyn <adobbs11@gmail.com>
2019-01-08 19:56:26 -0500

    rudimentary map

--- didnt_start_it.R
+++ didnt_start_it.R
@@ -22,7 +22,7 @@ addresses <-
   mutate(
     borough = str_extract(text, "^[^\\s]*\\s") %>% 
       str_remove("\\s"),
-    street = str_extract(text, "(\\*.+[,\\.])") %>% # All text after an asterisk and before a comma or period
+    street = str_extract(text, "(\\*[^\\.,]*)") %>% # All text after an asterisk and before a comma or period
       str_remove_all("(\\*.+\\*)") %>%  # Get rid of stuff in between asterisks
       str_trim()
   ) %>%
@@ -38,11 +38,27 @@ addresses <-
   mutate(
     address = na_if(address, "")
   ) %>% 
-  select(borough, street, address, text)
+  select(borough, street, address, text, created)
 
+# https://developers.google.com/maps/documentation/geocoding/intro#Geocoding
 geo_to_list <- function(inp) {
   geocode(inp) %>% list()
 }
+
+
+truncate_lat_long <- function(tbl, digits = 3) {
+  if (!"long" %in% names(tbl) ||
+      !"lat" %in% names(tbl)) {
+    stop("Input must contain columns lat and long.")
+  }
+  
+  tbl %>% 
+    mutate(
+      lat_trunc = round(lat, digits = digits),
+      long_trunc = round(long, digits = digits)
+    )
+}
+
 
 # Get lat lon
 lat_long <- 
@@ -50,13 +66,52 @@ lat_long <-
   rowwise() %>% 
   mutate(
     l_l = ifelse(is.na(address), tibble(lat = NA_character_, 
-                                        lon = NA_character_) %>% list(), 
+                                        long = NA_character_) %>% list(), 
                  geo_to_list(address))
   ) %>% 
   unnest() %>% 
-  select(address, lat, lon)
+  truncate_lat_long(digits = 1) %>% 
+  select(address, lat, long, lat_trunc, long_trunc, created, text)
 
 
+nyc <- 
+  ggplot2::map_data("state", region = "new york") %>%
+  truncate_lat_long(digits = 1) %>% 
+  as_tibble() 
+
+
+dat <- 
+  lat_long %>% 
+  rename(
+    lat_tweet = lat,
+    long_tweet = long
+  ) %>% 
+  left_join(nyc, by = c("lat_trunc", "long_trunc"))
+
+ggplot() +
+  geom_polygon(data = nyc, aes(lat, long)) +
+  geom_point(data = dat, aes(lat_tweet, long_tweet), color = "red") +
+  xlim(NA, 41) +
+  ylim(-75, -73)
+
+
+
+
+
+
+
+
+library(nycgeo) # https://github.com/mfherman/nycgeo
+library(sf)
+
+
+lat_long %>% 
+  drop_na(lat, long) %>% 
+  st_as_sf(coords = c("long", "lat"), 
+           crs = 4326, agr = "constant") %>% 
+  st_join(nyc_boundaries(geography = "block"))
+
+nyc_boundaries(geography = "block", add_acs_data = TRUE)$geometry[1]
 
 
 
